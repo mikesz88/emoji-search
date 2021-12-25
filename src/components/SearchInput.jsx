a3c25c08788a40e374949fec3830b7df24b835f4
michael <michaelsanchez8815@gmail.com>
2021-12-24 20:21:42 -0600

    update input field

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -12,8 +12,8 @@ class SearchInput extends React.Component {
     render() {
         return (
             <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignSelf: 'center'}}>
-                <input style={{height: '2rem', borderRadius: 8, fontSize: '2rem', padding: '1rem'}} onChange={this.updateChange} type="text" name="search" id="search"/>
-                <div>Click on the icon below to copy!</div>
+                <input style={{width: '100%', boxSizing: 'border-box', height: '2rem', borderRadius: 8, fontSize: '2rem', padding: '2rem'}} onChange={this.updateChange} type="text" name="search" id="search"/>
+                <div style={{textAlign: 'center'}}>Click on the icon below to copy!</div>
             </div>
         )
     }
